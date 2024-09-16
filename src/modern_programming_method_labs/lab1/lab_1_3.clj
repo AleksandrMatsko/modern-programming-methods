bbc462fe4fc5ac476d324060c824d3fbe284a9f2
AleksandrMatsko <AMatsko54@gmail.com>
2024-09-16 10:23:13 +0700

    feat: my filter for list

--- src/modern_programming_method_labs/lab1/lab_1_3.clj
+++ src/modern_programming_method_labs/lab1/lab_1_3.clj
@@ -1,14 +1,35 @@
 (ns modern-programming-method-labs.lab1.lab-1-3)
 
 (defn my-map-list
+  "Accepts f and coll and returns new collection,
+  each element of new collection is a result of applying f to corresponding element in coll.
+  f should accept one argument and return one argument."
   [f
    coll]
   (seq (reduce
-         (fn [arg1
-              arg2]
-           (concat arg1 (list (f arg2))))
+         (fn [accumulated
+              new-val]
+           (concat accumulated (list (f new-val))))
          (list)
          coll)))
 
 (println "map:" (map inc (list 1 2 3 4 5)))
 (println "my-map-list" (my-map-list inc (list 1 2 3 4 5)))
+
+(defn my-filter-list
+  "Accepts pred and coll and returns new collection,
+  consisted of elements from coll which satisfied pred.
+  pred should accept one argument and return true of false."
+  [predicate
+   coll]
+  (seq (reduce
+         (fn [accumulated
+              new-val]
+           (if (predicate new-val)
+             (concat accumulated (list new-val))
+             accumulated))
+         (list)
+         coll)))
+
+(println "filter:" (filter even? (list 1 2 3 4 5 6)))
+(println "my-filter-list:" (my-filter-list even? (list 1 2 3 4 5 6)))
--- test/modern_programming_method_labs/lab1/lab_1_3_test.clj
+++ test/modern_programming_method_labs/lab1/lab_1_3_test.clj
@@ -3,12 +3,22 @@
             [modern-programming-method-labs.lab1.lab-1-3 :refer :all]))
 
 (defn maps-equal
+  "Calls my-map-list and map with f and coll and compares results"
   [f
    coll]
   (= (my-map-list f coll) (map f coll)))
 
-(deftest my-map-with-inc-test
-  (testing "my-map and map res is equal with same function and collection"
+(defn filters-equal
+  "Calls my-filter-list and filter with pred and coll and compares results"
+  [pred
+   coll]
+  (= (my-filter-list pred coll) (filter pred coll)))
+
+(deftest my-map-list-equals-map-test
+  (testing "my-map-list and map res is equal with same function and collection"
     (is (maps-equal inc (list 1 2 3 4 5)))
     (is (maps-equal dec (list 1 2 3 4 5)))))
 
+(deftest my-filter-list-equals-filter-test
+  (testing "my-filter-list and filter res is equal with same predicate and collection"
+    (is (filters-equal even? (list 1 2 3 4 5 6)))))
