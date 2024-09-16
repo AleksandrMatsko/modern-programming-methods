3594c74b435980fc7efebc86c9300c17827f548d
AleksandrMatsko <AMatsko54@gmail.com>
2024-09-16 11:55:03 +0700

    feat: add tests to for core

--- test/modern_programming_method_labs/lab1/core_test.clj
+++ test/modern_programming_method_labs/lab1/core_test.clj
@@ -1,5 +1,6 @@
 (ns modern-programming-method-labs.lab1.core-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [modern-programming-method-labs.lab1.core :refer :all]))
 
 (defn contains-in-list
   [_list
@@ -31,3 +32,10 @@
 (def alphabet (list "a" "b" "c"))
 (def expected-words-with-len-2 (list "ab" "ac" "ba" "bc" "ca" "cb"))
 (def expected-words-with-len-3 (list "aba" "abc" "aca" "acb" "bab" "bac" "bca" "bcb" "cab" "cac" "cba" "cbc"))
+
+(deftest add-char-to-word-test
+  (testing "add char to word in different cases"
+    (is (nil? (add-char-to-word "abc" "a" 3)))
+    (is (= (add-char-to-word "" "a" 4) "a"))
+    (is (nil? (add-char-to-word "a" "a" 4)))
+    (is (= (add-char-to-word "a" "b" 4) "ab"))))
